b91cf60b4d3407adf5cd9c226877b22a7c8c92b6
bk <dubeybk15@gmail.com>
2019-09-06 00:36:09 +0530

    Adding kakfa fundamental project

---
+++ producer-consumer/src/main/java/co/kafka/consumer/SimpleConsumer.java
@@ -0,0 +1,34 @@
+package co.kafka.consumer;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.util.Arrays;
+import java.util.Properties;
+
+public class SimpleConsumer
+{
+    public static void main(String[] args)
+    {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "192.168.1.10:9101");
+        props.put("group.id", "grp-1");
+        props.put("enable.auto.commit", "true");
+        props.put("auto.commit.interval.ms", "1000");
+        props.put("session.timeout.ms", "30000");
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+
+        consumer.subscribe(Arrays.asList("kafka_topic"));
+        while (true)
+        {
+            ConsumerRecords<String, String> records = consumer.poll(1000);
+            for (ConsumerRecord<String, String> record : records)
+            {
+                System.out.println("=============== partition Id= " + record.partition() + "  offset = " + record.offset() + " value = " + record.value() + "=================");
+            }
+        }
+    }
+}
